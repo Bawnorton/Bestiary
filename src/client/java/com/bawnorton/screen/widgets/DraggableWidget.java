35daa3ecb7e94614f2c62aa67cc3cbaa487df408
Benjamin-Norton <bawnorton@me.com>
2023-01-29 20:58:16 -0800

    Add draggable widget

--- src/client/java/com/bawnorton/screen/widgets/DraggableWidget.java
+++ src/client/java/com/bawnorton/screen/widgets/DraggableWidget.java
@@ -1,2 +1,42 @@
-package com.bawnorton.screen.widgets;public class DraggableWidget {
+package com.bawnorton.screen.widgets;
+
+import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
+import net.minecraft.client.gui.widget.ClickableWidget;
+import net.minecraft.client.gui.widget.PressableWidget;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.Text;
+
+public class DraggableWidget extends ClickableWidget {
+    private final ClickableWidget interactable;
+
+    public DraggableWidget(int x, int y, int width, int height, Text text, ClickableWidget interactable) {
+        super(x, y, width, height, text);
+        this.interactable = interactable;
+    }
+
+    @Override
+    public void renderButton(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        super.renderButton(matrices, mouseX, mouseY, delta);
+    }
+
+    @Override
+    public void onClick(double mouseX, double mouseY) {
+        this.interactable.onClick(mouseX, mouseY);
+    }
+
+    @Override
+    public void onRelease(double mouseX, double mouseY) {
+        this.interactable.onRelease(mouseX, mouseY);
+    }
+
+    @Override
+    protected void appendClickableNarrations(NarrationMessageBuilder builder) {
+        this.appendDefaultNarrations(builder);
+    }
+
+    @Override
+    protected void onDrag(double mouseX, double mouseY, double deltaX, double deltaY) {
+        this.setX((int) mouseX);
+        this.setY((int) mouseY);
+    }
 }
